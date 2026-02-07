19bf539b05e9d1f142c097db2560965528cc003f
Luis Jung <luis007jung@gmail.com>
2026-02-07 16:30:45 -0600

    my primer commit

--- calculadora.py
+++ calculadora.py
@@ -13,29 +13,36 @@ while True:
 
     opcion = input("Ingresa el número de la opción: ")
 
-    if opcion == "6":
-        num = float(input("Ingresa el número: "))
-        if num >= 0:
-            print("Resultado:", math.sqrt(num))
-        else:
-            print("Error: no se puede calcular la raíz de un número negativo")
+    if opcion not in ["1", "2", "3", "4", "5", "6"]:
+        print("Error: opción no válida")
         continue
 
-    num1 = float(input("Ingresa el primer número: "))
-    num2 = float(input("Ingresa el segundo número: "))
+    try:
+        if opcion == "6":
+            num = float(input("Ingresa el número: "))
+            if num < 0:
+                print("Error: no se puede calcular la raíz de un número negativo")
+            else:
+                print("Resultado:", math.sqrt(num))
+            continue
 
-    if opcion == "1":
-        print("Resultado:", num1 + num2)
-    elif opcion == "2":
-        print("Resultado:", num1 - num2)
-    elif opcion == "3":
-        print("Resultado:", num1 * num2)
-    elif opcion == "4":
-        if num2 != 0:
-            print("Resultado:", num1 / num2)
-        else:
-            print("Error: no se puede dividir entre cero")
-    elif opcion == "5":
-        print("Resultado:", num1 ** num2)
-    else:
-        print("Opción no válida")
+        num1 = float(input("Ingresa el primer número: "))
+        num2 = float(input("Ingresa el segundo número: "))
+
+        if opcion == "1":
+            print("Resultado:", num1 + num2)
+        elif opcion == "2":
+            print("Resultado:", num1 - num2)
+        elif opcion == "3":
+            print("Resultado:", num1 * num2)
+        elif opcion == "4":
+            if num2 == 0:
+                print("Error: no se puede dividir entre cero")
+            else:
+                print("Resultado:", num1 / num2)
+        elif opcion == "5":
+            print("Resultado:", num1 ** num2)
+
+    except ValueError:
+        print("Error: debes ingresar números válidos")
+
