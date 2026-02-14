91c53767a15bf15c001d9aea1c0cc6b1d2841565
Luis Jung <luis007jung@gmail.com>
2026-02-14 16:59:44 -0600

    agregue una nueva linea

--- calculadora.py
+++ calculadora.py
@@ -31,6 +31,7 @@ while True:
                 print("Resultado:", math.sqrt(num))
             continue
 
+
         num1 = float(input("Ingresa el primer número: "))
         num2 = float(input("Ingresa el segundo número: "))
 
